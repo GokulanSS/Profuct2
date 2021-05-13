cd3261d04bd7b6222f3a4e63d20b7f3185a71b39
Gokulan <gokulanqwe1@gmail.com>
2021-05-13 15:32:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>Product Name</h1>
+        <input type="text" name="name" />
+       <br></br>
+        <h1>Product Price</h1>
+        <input type="number" name="price" />
+        <br></br>
+        <button>
+        Save
+       </button>
+       </header>
+      
+    </div>
+  );
+}
+
+export default App;
